5b6f5b2cf67eb26aacdcd6ce2776559283118206
Abel PASTOR ORTS <apastorts@gmail.com>
2018-12-10 19:40:49 +1100

    Completing style

--- resources/js/components/MainForm.vue
+++ resources/js/components/MainForm.vue
@@ -9,6 +9,24 @@
              Treasures From God's Word
            </div>
            <div class="flex justify-between text-lg">
+             <div class="p-2 font-bold block">Treasures Talk</div>
+             <div class="p-2 font-bold block">
+               <div class="bibleReading-selection inline-block">
+                 {{talk.name}}
+               </div>
+               <select-user :users=users v-model="talk"></select-user>
+             </div>
+           </div>
+           <div class="flex justify-between text-lg">
+             <div class="p-2 font-bold block">Digging For Spiritual Gems</div>
+             <div class="p-2 font-bold block">
+               <div class="bibleReading-selection inline-block">
+                 {{digging.name}}
+               </div>
+               <select-user :users=users v-model="digging"></select-user>
+             </div>
+           </div>
+           <div class="flex justify-between text-lg">
              <div class="p-2 font-bold block">Bible Reading</div>
              <div class="p-2 font-bold block">
                <div class="bibleReading-selection inline-block">
@@ -24,9 +42,69 @@
            <div class="apply-yourself p-2 text-center text-4xl">
              Apply Yourself To The Field Ministry
            </div>
+           <div class="flex justify-between text-lg">
+             <div class="p-2 font-bold block">Initial Call</div>
+             <div class="p-2 font-bold block">
+               <div class="bibleReading-selection inline-block">
+                 {{initial.name}}
+               </div>
+               <select-user :users=users v-model="initial"></select-user>
+             </div>
+           </div>
+           <div class="flex justify-between text-lg">
+             <div class="p-2 font-bold block">First Return Visit</div>
+             <div class="p-2 font-bold block">
+               <div class="firstRV-selection inline-block">
+                 {{firstRV.name}}
+               </div>
+               <select-user :users=users v-model="firstRV"></select-user>
+             </div>
+           </div>
+           <div class="flex justify-between text-lg">
+             <div class="p-2 font-bold block">Bible Study / Talk</div>
+             <div class="p-2 font-bold block">
+               <div class="talk-selection inline-block">
+                 {{talk.name}}
+               </div>
+               <select-user :users=users v-model="talk"></select-user>
+             </div>
+           </div>
          </div>
        </div>
-
+       <div class="row">
+         <div class="col-md-8">
+           <div class="living p-2 text-center text-4xl">
+             Living As Christians
+           </div>
+           <div class="flex justify-between text-lg">
+             <div class="p-2 font-bold block">Local Needs / Talk</div>
+             <div class="p-2 font-bold block">
+               <div class="talk-selection inline-block">
+                 {{localNeeds.name}}
+               </div>
+               <select-user :users=users v-model="localNeeds"></select-user>
+             </div>
+           </div>
+           <div class="flex justify-between text-lg">
+             <div class="p-2 font-bold block">2nd Part</div>
+             <div class="p-2 font-bold block">
+               <div class="talk-selection inline-block">
+                 {{secondPart.name}}
+               </div>
+               <select-user :users=users v-model="secondPart"></select-user>
+             </div>
+           </div>
+           <div class="flex justify-between text-lg">
+             <div class="p-2 font-bold block">Congregation Bible Study</div>
+             <div class="p-2 font-bold block">
+               <div class="talk-selection inline-block">
+                 {{congregationBible.name}}
+               </div>
+               <select-user :users=users v-model="congregationBible"></select-user>
+             </div>
+           </div>
+         </div>
+       </div>
     </div>
 </template>
 
@@ -42,7 +120,31 @@
             users: '',
             bibleReading: {
               name: 'Not Assigned'
-            }
+            },
+            talk: {
+              name: 'Not Assigned'
+            },
+            digging: {
+              name: 'Not Assigned'
+            },
+            initial: {
+              name: 'Not Assigned'
+            },
+            firstRV: {
+              name: 'Not Assigned'
+            },
+            talk: {
+              name: 'Not Assigned'
+            },
+            congregationBible: {
+              name: 'Not Assigned'
+            },
+            secondPart: {
+              name: 'Not Assigned'
+            },
+            localNeeds: {
+              name: 'Not Assigned'
+            },
           }
         },
         mounted() {
