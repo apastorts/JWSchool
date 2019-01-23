d70cdcc1b65d1696f326602ab661b520d1c0f712
Abel Pastor <apastorts@gmail.com>
2019-01-23 21:18:03 +0000

    Update mail.php with correct from mail

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@the-goodson.com'),
+        'name' => env('MAIL_FROM_NAME', 'Abel Pastor Orts'),
     ],
 
     /*
